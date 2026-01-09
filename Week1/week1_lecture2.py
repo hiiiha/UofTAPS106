be888c7ec1acb56e9e721964a1904cc0fd57e97a
hiiiha <avenzhao@yandex.com>
2026-01-08 21:03:09 -0500

    First commit new week 1 files

---
+++ Week1/week1_lecture2.py
@@ -0,0 +1,9 @@
+# Running Your First Python (.py) File
+
+name_of_student = 'Aven' #replace the name inside the quotation marks with your name
+
+# Press the green triangle-shaped button to run this python file.
+
+print("\n\n\nCongratulations to", name_of_student, "for executing your first .py file in APS106!\n\n\n")
+
+# Look below (in the "Run" tab) for the output
